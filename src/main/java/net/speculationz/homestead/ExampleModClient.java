db637a114940ecefec7ab628329e87e8c3884208
Speculationz <isaac@hawkinsdigital.net>
2025-10-09 23:45:00 +1100

    Updated 09/10/2025

---
+++ src/main/java/net/speculationz/homestead/ExampleModClient.java
@@ -0,0 +1,28 @@
+package net.speculationz.homestead;
+
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = Homestead.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = Homestead.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ExampleModClient {
+
+    public ExampleModClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Some client setup code
+        Homestead.LOGGER.info("HELLO FROM CLIENT SETUP");
+        Homestead.LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+    }
+}
